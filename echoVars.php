3ede1ee5605a71db776b637f02c6aa5d66ed038c
tigerfarm <tigerfarm@gmail.com>
2018-06-19 17:00:51 -0700

    updates

--- echoVars.php
+++ echoVars.php
@@ -4,7 +4,9 @@ echo "\xA+++ Echo environment variables.";
 //
 $tokenHost = getenv("TOKEN_HOST");
 $tokenClientId = getenv('CLIENT_ID');
+$tokenPassword = getenv('TOKEN_PASSWORD');
 echo "\xA", "+ TOKEN_HOST   : ", $tokenHost;
+echo "\xA", "+ TOKEN_PASSWORD   : ", $tokenPassword;
 echo "\xA", "+ CLIENT_ID    : ", $tokenClientId;
 //
 echo "\xA--------------------------------------- \xA";
